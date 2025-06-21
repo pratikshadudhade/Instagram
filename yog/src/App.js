4bce40836266045c8518d4deb526061ac0ba07e5
dudhadepratiksha242@gmail.com <dudhadepratiksha242@gmail.com>
2025-06-21 23:39:35 +0530

    commit

--- yog/src/App.js
+++ yog/src/App.js
@@ -1,15 +1,15 @@
 import './App.css';
 // import './Components/Loginsingnup/Login.css'
 // import Login from './Components/Loginsingnup/Login';
-// import Instagram from './Components/Loginsingnup/Instagram';
-import Calculator from './Components/Loginsingnup/Calculator'
+import Instagram from './Components/Loginsingnup/Instagram';
+// import Calculator from './Components/Loginsingnup/Calculator'
 import './Components/Loginsingnup/Instagram.css'
 function App() {
   return (
     <div>
-      <Calculator/>
+      {/* <Calculator/> */}
       {/* <Login/> */}
-      {/* <Instagram/> */}
+      <Instagram/>
     </div>
   );
 }
